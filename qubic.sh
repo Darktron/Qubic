bd0323918b847012109860484d9b48bc01859525
Darktron <omyp12@gmail.com>
2024-04-02 08:22:19 -0400

    Update qubic.sh

--- qubic.sh
+++ qubic.sh
@@ -46,8 +46,9 @@ download_latest_release() {
 repo_owner="Qubic-Solutions"
 repo_name="rqiner-builds"
 file_path="rqiner-aarch64-mobile"
-download_location="~/qubic"
+download_location="/qubic"
 
 run_update_and_upgrade
 install_packages
 download_latest_release "$repo_owner" "$repo_name" "$file_path" "$download_location"
+cd ~/qubic
