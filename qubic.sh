d14f678fe18b3fb7558e7ae22c40078af07c8bf1
Darktron <omyp12@gmail.com>
2024-04-02 07:09:14 -0400

    Create qubic.sh

---
+++ qubic.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+run_update_and_upgrade() {
+    echo "Running 'apt update'..."
+    yes | sudo apt update
+
+    echo "Running 'apt upgrade'..."
+    yes | sudo apt upgrade
+}
+
+download_latest_release() {
+    local repo_owner=$1
+    local repo_name=$2
+    local file_path=$3
+    local download_location=$4
+
+    # Create directory if it doesn't exist
+    mkdir -p "$(dirname "$download_location")"
+
+    if [ -f "$download_location" ]; then
+        
+        local filename=$(basename "$download_location")
+        
+        mv "$download_location" "$(dirname "$download_location")/${filename}.old"
+        echo "Existing file '$filename' renamed to '${filename}.old'"
+    fi
+
+    local release_info=$(curl -s "https://api.github.com/repos/${repo_owner}/${repo_name}/releases/latest")
+
+    local binary_url=$(echo "$release_info" | jq -r ".assets[] | select(.name == \"$file_path\") | .browser_download_url")
+
+    if [ -z "$binary_url" ]; then
+        echo "Binary file '$file_path' not found in the latest release"
+        exit 1
+    fi
+
+    curl -L -o "$download_location" -C - "$binary_url"
+
+    echo "Latest release miner file '$file_path' downloaded to '$download_location'"
+}
+
+repo_owner="Qubic-Solutions"
+repo_name="rqiner-builds"
+file_path="rqiner-aarch64-mobile"
+download_location="~/qubic"
+
+run_update_and_upgrade
+download_latest_release "$repo_owner" "$repo_name" "$file_path" "$download_location"
